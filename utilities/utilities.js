848660e1a8418e3eaa0c91f7664db93d74a7f5c3
shad910 <shadshs91@gmail.com>
2025-03-14 22:51:43 +0600

    first commit and basic setup

---
+++ utilities/utilities.js
@@ -0,0 +1,3 @@
+const getID = (id) => document.getElementById(id);
+const getCLASS = (className) => document.getElementsByClassName(className);
+const createElement = (element) => document.createElement(element); 
